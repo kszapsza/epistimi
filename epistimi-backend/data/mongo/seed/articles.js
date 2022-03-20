3b7cac0b6d2f78f848b0b42214436bc54f1f5c34
Karol Szapsza <kszapsza@gmail.com>
2022-03-20 18:55:34 +0100

    seed mongodb scripts

---
+++ epistimi-backend/data/mongo/seed/articles.js
@@ -0,0 +1,38 @@
+db.articles.insertMany([
+  {
+    _id: ObjectId('6209003ebfa894e629aa6079'),
+    title: 'Laborum magna do magna velit magna sint adipisicing',
+    description: 'Do do dolor minim minim dolore velit occaecat officia adipisicing laborum culpa enim laboris officia id excepteur incidunt dolore excepteur enim mollit nulla nulla ut',
+    slug: 'laborum-magna-do-magna-velit-magna-sint-adipisicing'
+  },
+  {
+    _id: ObjectId('6209003ebfa894e629aa607a'),
+    title: 'Fugiat culpa nostrud reprehenderit est ad minim officia',
+    description: 'Ex esse cupatat culpa consequat eiusmod esse aliquip laborum dolore duis quis elit occaecat elit amet id occaecat consectetur laborum deserunt mollit officia labore ex',
+    slug: 'fugiat-culpa-nostrud-reprehenderit-est-ad-minim-officia'
+  },
+  {
+    _id: ObjectId('6209003ebfa894e629aa607b'),
+    title: 'Elit in cillum excepteur amet exercitation occaecat cillum',
+    description: 'Aliquip fugiat ea exercitation labore in consequat laborum commodo velit laboris enim dolor quis id velit consequat deserunt deserunt eiusmod voluptate velit',
+    slug: 'elit-in-cillum-excepteur-amet-exercitation-occaecat-cillum'
+  },
+  {
+    _id: ObjectId('6209003ebfa894e629aa607c'),
+    title: 'Aliquip velit exercitation consectetur ipsum occaecat dolore nulla elit',
+    description: 'Ut ea id nulla et do pariatur qui ut qui duis officia eu amet reprehenderit aliqua ipsum Lorem nulla aliqua ipsum reprehenderit cillum',
+    slug: 'aliquip-velit-exercitation-consectetur-ipsum-occaecat-dolore-nulla-elit'
+  },
+  {
+    _id: ObjectId('6209003ebfa894e629aa607d'),
+    title: 'Culpa elit deserunt quis et et veniam ipsum exercitation excepteur aliquip',
+    description: 'Officia reprehenderit nisi quis sunt adipisicing et proident sunt nisi sit nulla non officia ut officia excepteur ut esse duis occaecat Lorem ex et',
+    slug: 'culpa-elit-deserunt-quis-et-et-veniam-ipsum-exercitation-excepteur-aliquip'
+  },
+  {
+    _id: ObjectId('6209003ebfa894e629aa607e'),
+    title: 'Non amet nulla ad minim nulla reprehenderit mollit',
+    description: 'Officia Lorem consectetur veniam labore sit ea elit consequat aute et esse ipsum dolor ipsum id culpa quis aute tempor fugiat esse dolor',
+    slug: 'non-amet-nulla-ad-minim-nulla-reprehenderit-mollit'
+  },
+]);
